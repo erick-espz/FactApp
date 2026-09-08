dacfcd41607a66ddf8da3bf1f230fb27d2233585
Erick Arana <eaarana@uamv.edu.ni>
2026-09-08 16:21:35 -0600

    update (Cargo, Empleado)

--- src/main/java/com/tuempresa/fact_app/model/Cargo.java
+++ src/main/java/com/tuempresa/fact_app/model/Cargo.java
@@ -1,4 +1,16 @@
 package com.tuempresa.fact_app.model;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
 public class Cargo {
+
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+
 }
--- src/main/java/com/tuempresa/fact_app/model/Empleado.java
+++ src/main/java/com/tuempresa/fact_app/model/Empleado.java
@@ -1,4 +1,19 @@
 package com.tuempresa.fact_app.model;
 
+import lombok.*;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Empleado {
+
+    private Integer id;
+    private String nombres;
+    private String apellidos;
+    private Cargo cargo;
+    private LocalDate fechaContratacion;
+    private boolean activo;
+
 }
